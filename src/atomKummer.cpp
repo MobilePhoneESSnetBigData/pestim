c0eb83e37dc5d59c0b466ad6b58c7886614f0642
Bogdan Oancea <bogdanoancea@Bogdans-MacBook-Pro.local>
2018-01-30 06:36:57 +0200

    further optimizations

--- src/atomKummer.cpp
+++ src/atomKummer.cpp
@@ -2,13 +2,15 @@
 using namespace Rcpp;
 //[[Rcpp::export]]
 NumericVector atomKummer(NumericVector x, NumericVector a, NumericVector b, double relTol) {
-  int n;
-  int l = x.size();
-  NumericVector sumando(l), suma(l);
+  long n;
+  long dim = x.size();
+  NumericVector sumando(dim,1.), suma(dim,1.);
 
   long j;
-  for(j=0;j<l;j++)
-    for(n=1, sumando[j]=1, suma[j]=1; sumando[j] / suma[j] > relTol; ++n, sumando[j] *= x[j] * (a[j]+n-1) / ((b[j]+n-1)*n), suma[j] +=sumando[j]);
-
+  for(j=0;j<dim;j++) {
+    a[j]--;
+    b[j]--;
+    for(n=1; sumando[j] / suma[j] > relTol; ++n, sumando[j] *= x[j] * (a[j]+n) / ((b[j]+n)*n), suma[j] +=sumando[j]);
+  }
   return(suma);
 }
