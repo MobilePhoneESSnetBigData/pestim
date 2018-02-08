a88b637cf6e0de18ff13aca584bbbd3353f59b2a
david-salgado <david.salgado.fernandez@gmail.com>
2018-02-08 18:51:48 +0100

    Shared

--- R/kummer.R
+++ R/kummer.R
@@ -1,20 +1,13 @@
 #' @title kummmer
 #' @description
 #' @author David Salgado, Bogdan Oancea
+#' @useDynLib pestim
 #' @export
 #'
-kummer2 <- function(x, a, b, relTol = 1e-6){
-
-  output <- sapply(seq(along = x), function(i){atomKummer(x[i], a[i], b[i], relTol)})
-  output[is.infinite(output)] <- .Machine$double.xmax
-  return(output)
-
-}
-
 kummer <- function(x, a, b, relTol = 1e-6){
-  
+
   output <- atomKummer(x, a, b, relTol)
   output[is.infinite(output)] <- .Machine$double.xmax
   return(output)
-  
+
 }
