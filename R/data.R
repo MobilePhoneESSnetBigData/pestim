d6c10abd3d883e636585130cb2848aef8e8a3bc6
david-salgado <david.salgado.fernandez@gmail.com>
2018-02-08 10:40:13 +0100

    Document data

--- R/data.R
+++ R/data.R
@@ -1,26 +1,19 @@
-#' True population in each cell at each time step.
+#' Dataset with simulated data for population counts.
 #'
-#' A dataset containing the true population for a set of cells for succesive moments of time.
+#' This dataset provides population counts moving from each pair of cells at succesive time instants
+#'  for a simulated true population, a corresponding official population in a register and a
+#'  population detected with a mobile telecommunication network.
 #'
-#' @format A data table with 60 rows and 3 variables:
+#' @format A \linkS4class{data.table} with 96912 rows and 6 variables:
 #' \describe{
-#'   \item{Time}{\code{int}, time steps for which we want to estimate the population in each cell}
-#'   \item{Cell}{\code{factor}, the cell number for which we estimate the population}
-#'   \item{N0True}{\code{numeric}, the true population in each cell, at each time moment}
+#'   \item{ID_CELL_INI}{identification code for each initial cell in the displacements}
+#'   \item{ID_CELL_END}{identification code for each final cell in the displacements}
+#'   \item{ID_T}{identification code of each time moment. It is very important to underline that the
+#'   table collects always displacements between the initial time instant and the corresponding time
+#'   instant specified by \code{ID_T}}
+#'   \item{N_REG}{counts according to the population register. Note that these counts do not evolve
+#'   in time}
+#'   \item{N_0}{counts of the simulated true population}
+#'   \item{N_MNO_1}{counts of individuals detected by the Mobile Network Operator}
 #' }
-"TruePop"
-
-#' Simulated population mobility.
-#'
-#' A dataset containing simulated population mobility from one cell to another cell.
-#'
-#' @format A data table with 760 rows and 6 variables:
-#' \describe{
-#'   \item{ID_CELL_INI}{\code{int}, time steps for which we want to estimate the population in each cell}
-#'   \item{ID_CELL_FIN}{\code{int}, time steps for which we want to estimate the population in each cell}
-#'   \item{ID_T}{\code{int}, time steps for which we want to estimate the population in each cell}
-#'   \item{N_REG}{\code{numeric}, the number of population in each cell from the population administrative register}
-#'   \item{N0}{\code{numeric}, the initial number of population in each cell}
-#'   \item{N_MNO_1}{\code{numeric}, the number of population as given by the mobile phone operator}
-#' }
-"Sim.PopMob"
+"MobPop"
