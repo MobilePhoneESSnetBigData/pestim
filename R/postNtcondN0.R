3058cbce1a040dbdd4f03fd988e1cae9e5f3318a
david-salgado <david.salgado.fernandez@gmail.com>
2018-02-08 14:46:16 +0100

    Document function postNtcondN0

--- R/postNtcondN0.R
+++ R/postNtcondN0.R
@@ -1,20 +1,40 @@
-#' @title Time evolution of the posterior mean, median, and mode for the number of individuals conditional on their estimated initial values
-#' @description Compute the time evolution of the posterior mean, median, and mode for the number of individuals
-#' conditional on their estimated initial values, according to the hierarchical model.
-#' @author David Salgado
+#' @title Posterior mean, median, and mode for the number of individuals at an arbitrary time
+#' conditioned upon the initial population.
 #'
-#' @param N0 initial number of population in each cell
+#' @description Compute the posterior mean, median, and mode for the number of individuals
+#' generating posterior distribution according to the hierarchical model conditioned upon the
+#' initial population of each cell, which must be provided
 #'
-#' @param nMNOmat
+#' @param N0 initial population in each cell
 #'
-#' @param distNames a list of prior distribution names. It could contain the following names: 'unif', 'degen', 'triang' or 'gamma'
+#' @param nMNOmat transition matrix with the number of individuals displaced from cell to cell
+#' detected by the Mobile Network Operator
 #'
-#' @param variation
+#' @param distNames character vector with the names of the prior distributions for each cell
+#'
+#' @param variation list of lists whose components are parameters providing a measure of variation
+#' of each prior distribution
 #'
 #' @param n number of points to generate in the posterior distribution for the computation. Default
 #' value is 1e3
 #'
 #' @return
+#'
+#' @examples
+#' ## First, the inputs:
+#'
+#' # The initial population
+#' N0 <- c(93, 123, 130)
+#'
+#' #The transition matrix of individuals detected by the MNO
+#' nMNOmat <- rbind(c(10, 3, 4), c(5, 21, 3), c(3, 9, 18))
+#'
+#' # Names and parameters of priors for the transition probabilities
+#' distNames <- rep('unif', 3)
+#' variation <- rep(list(list(cv = 0.20)), 3)
+#'
+#' # It takes a couple of minutes.
+#' postNtcondN0(N0, nMNOmat, distNames, variation)
 #'
 #' @include rNtcondN0.R
 #'
