6b59d28561801b52b95d18a13c3ecf9eb8fe5044
bogdanoancea <bogdan.oancea@gmail.com>
2018-03-06 12:48:18 +0200

    a bug removed from pKuumer.cpp

--- src/pKummer.cpp
+++ src/pKummer.cpp
@@ -1,42 +1,38 @@
-// [[Rcpp::depends(RcppParallel)]]
-#include "RcppParallel.h"
 #include "Rcpp.h"
+#include "RcppParallel.h"
 #include <cmath>
 using namespace RcppParallel;
 
 
-
+// [[Rcpp::depends(RcppParallel)]]
 struct wKummer : public Worker {
-  
+
   //input
   const RVector<double> z;
   const RVector<double> a;
   const RVector<double> b;
   const double relTol;
-  
+
   // output
   RVector<double> suma;
-  
+
   // initialize from Rcpp input and output
-  wKummer(Rcpp::NumericVector z, Rcpp::NumericVector a, Rcpp::NumericVector b, double relTol, Rcpp::NumericVector suma)
-    : z(z), a(a), b(b), relTol(relTol), suma(suma) {}
-  
+  wKummer(Rcpp::NumericVector z1, Rcpp::NumericVector a1, Rcpp::NumericVector b1, double relTol1, Rcpp::NumericVector suma1)
+    : z(z1), a(a1), b(b1), relTol(relTol1), suma(suma1) {}
+
   // function call operator that work for the specified range (begin/end)
   void operator()(std::size_t begin, std::size_t end) {
+    int j;
+    double sumando;
     for (std::size_t i = begin; i < end; i++) {
-      suma[i] = 1;
-      double sumando = 1;
-      //double dif = a[i] - b[i];
-      //double dif2 = 1- a[i];
-      
       if (z[i]<80) {
-        for (long j=0;std::fabs(sumando/suma[i])>relTol;++j){
+        for (j=0,sumando=1,suma[i]=1;fabs(sumando/suma[i])>relTol;++j){
           sumando*=(z[i] / (j+1)) * ((a[i] + j) / (b[i] + j));
           suma[i]+=sumando;
         }
       }
       else {
-        for (long j=0;fabs(sumando/suma[i])>relTol;++j){
+        for (j=0,sumando=1,suma[i]=1;fabs(sumando/suma[i])>relTol;++j){
           sumando*=((1-a[i]+j) / (j+1)) * ((b[i] - a[i] + j) / (z[i]));
           suma[i]+=sumando;
         }
@@ -50,11 +46,11 @@ struct wKummer : public Worker {
 Rcpp::NumericVector pKummer(Rcpp::NumericVector z, Rcpp::NumericVector a, Rcpp::NumericVector b, double relTol) {
   int n=z.size();
   Rcpp::NumericVector suma(n);
-  
+
   wKummer wk(z, a, b, relTol, suma);
-  
+
   // call it with parallelFor
   parallelFor(0, z.length(), wk);
-  
+
   return(suma);
 }
