8be02f5884f546fb8afdeae7743602b8f65a697e
bogdanoancea <bogdan.oancea@gmail.com>
2018-04-10 10:40:33 +0300

    improved functions modeLambda and rlambda

--- R/triang.R
+++ R/triang.R
@@ -15,17 +15,15 @@
 #'
 #' @param n number of observations
 #'
-#' @param xMin vector with the minimum values of the range of the random variable
+#' @param xMin  the minimum values of the range of the random variable
 #'
-#' @param xMax vector with the maximum values of the range of the random variable
+#' @param xMax the maximum values of the range of the random variable
 #'
-#' @param xMode vector with the modes of the random variable
+#' @param xMode the mode of the random variable
 #'
 #' @return \code{dtriang} gives the density, \code{ptriang} gives the distribution function,
 #' \code{qtriang} gives the quantile function, and \code{rtriang} generates random deviates.
 #'
-#' The lengths of the input vectors (except \code{n}) must be all equal except when their length is
-#' 1. Otherwise \code{NA}s are produced.
 #'
 #' @seealso \code{\link{Distributions}} for other distributions
 #'
@@ -43,73 +41,59 @@
 #' @export
 dtriang <- function(x, xMin, xMax, xMode){
 
-  if (any(xMin > xMax)) stop('xMax must be greater than xMin.')
-  if (!(all(xMode >= xMin) & all(xMode <= xMax))) stop('xMode must be between xMin and xMax.')
+  if (xMin > xMax) stop('xMax must be greater than xMin.')
+  if (!((xMode >= xMin) & (xMode <= xMax))) stop('xMode must be between xMin and xMax.')
 
-
-  n <- length(x)
-  if (length(xMin) == 1) xMin <- rep(xMin, n)
-  if (length(xMax) == 1) xMax <- rep(xMax, n)
-  if (length(xMode) == 1) xMode <- rep(xMode, n)
 
   output <- x
   output[x <= xMin | x >= xMax] <- 0
   range1 <- (x > xMin & x <= xMode)
-  output[range1] <- (2 * (x[range1] - xMin[range1])) / ((xMax[range1] - xMin[range1]) * (xMode[range1] - xMin[range1]))
+  output[range1] <- (2 * (x[range1] - xMin)) / ((xMax - xMin) * (xMode - xMin))
   range2 <- (x >= xMode & x < xMax)
-  output[range2] <- (2 * (xMax[range2] - x[range2])) / ((xMax[range2] - xMin[range2]) * (xMax[range2] - xMode[range2]))
+  output[range2] <- (2 * (xMax - x[range2])) / ((xMax - xMin) * (xMax - xMode))
   return(output)
 }
+
 
 #' @rdname triang
 #' @export
 ptriang <- function(q, xMin, xMax, xMode){
 
-  if (any(xMin > xMax)) stop('xMax must be greater than xMin.')
-  if (!(all(xMode >= xMin) & all(xMode <= xMax))) stop('xMode must be between xMin and xMax.')
-
-  n <- length(q)
-  if (length(xMin) == 1) xMin <- rep(xMin, n)
-  if (length(xMax) == 1) xMax <- rep(xMax, n)
-  if (length(xMode) == 1) xMode <- rep(xMode, n)
+  if (xMin > xMax) stop('xMax must be greater than xMin.')
+  if (!((xMode >= xMin) & (xMode <= xMax))) stop('xMode must be between xMin and xMax.')
 
   output <- q
   output[q <= xMin] <- 0
   output[q >= xMax] <- 1
   range1 <- (q > xMin & q <= xMode)
-  output[range1] <- ((output[range1] - xMin[range1])^2) / ((xMax[range1] - xMin[range1]) * (xMode[range1] - xMin[range1]))
+  output[range1] <- ((output[range1] - xMin)^2) / ((xMax - xMin) * (xMode - xMin))
   range2 <- (q > xMode & q < xMax)
-  output[range2] <- 1 - ((output[range2] - xMax[range2])^2) / ((xMax[range2] - xMin[range2]) * (xMax[range2] - xMode[range2]))
+  output[range2] <- 1 - ((output[range2] - xMax)^2) / ((xMax - xMin) * (xMax - xMode))
   return(output)
 }
+
 
 #' @rdname triang
 #' @export
 qtriang <- function(q, xMin, xMax, xMode){
 
-  if (any(xMin > xMax)) stop('xMax must be greater than xMin.')
-  if (!(all(xMode >= xMin) & all(xMode <= xMax))) stop('xMode must be between xMin and xMax.')
+  if (xMin > xMax) stop('xMax must be greater than xMin.')
+  if (!((xMode >= xMin) & (xMode <= xMax))) stop('xMode must be between xMin and xMax.')
 
-  n <- length(q)
-  if (length(xMin) == 1) xMin <- rep(xMin, n)
-  if (length(xMax) == 1) xMax <- rep(xMax, n)
-  if (length(xMode) == 1) xMode <- rep(xMode, n)
 
   output <- q
   range1 <- (q < (xMode - xMin) / (xMax - xMin))
-  output[range1] <- xMin[range1] + sqrt(q[range1] * (xMax[range1] - xMin[range1]) * (xMode[range1] - xMin[range1]))
+  output[range1] <- xMin + sqrt(q[range1] * (xMax - xMin) * (xMode - xMin))
   range2 <- ( q > (xMode - xMin) / (xMax - xMin))
-  output[range2] <- xMax[range2] - sqrt((1 - q[range2]) * (xMax[range2] - xMin[range2]) * (xMax[range2] - xMode[range2]))
+  output[range2] <- xMax - sqrt((1 - q[range2]) * (xMax - xMin) * (xMax - xMode))
   return(output)
 }
 
 #' @rdname triang
 #' @export
 rtriang <- function(n, xMin, xMax, xMode){
-
-  if (any(xMin > xMax)) stop('xMax must be greater than xMin.')
-  if (!(all(xMode >= xMin) & all(xMode <= xMax))) stop('xMode must be between xMin and xMax.')
-
+  if (xMin > xMax) stop('xMax must be greater than xMin.')
+  if (!((xMode >= xMin) & (xMode <= xMax))) stop('xMode must be between xMin and xMax.')
   u <- runif(n)
   mc <- match.call()
   mc[[1L]] <- qtriang
