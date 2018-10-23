9d0b885324506fdb33e239ee3d28adfb3dc88f3f
Bogdan Oancea <Bogdan.Oancea@insse.ro>
2018-10-23 16:33:13 +0300

    small changes

--- inst/tests/test_triang.R
+++ inst/tests/test_triang.R
@@ -7,11 +7,11 @@ test_that("qtriang", {
   mode = rnorm(1,15)
   xmin = rnorm(1,10)
   xmax = rnorm(1,25)
-  expected_result = c(10.18364332422208, 11.89504468813396, 12.60393034373007, 13.14787743866012, 13.60644605204583,
-                      14.01045311071489, 14.37570420407118, 14.73177566854184, 15.10182655910430, 15.48764530354077,
-                      15.89143766931765, 16.31597734318609, 16.76483452062717, 17.24273844334560, 17.75618448456067,
-                      18.31452385509277, 18.93210869132071, 19.63309897334713, 20.46460295410856, 21.54824003945533,
-                      24.16437138758995)
+  expected_result = c(10.183643324222082, 11.895044688133956, 12.603930343730067, 13.147877438660121, 13.606446052045831, 14.010453110714888,
+                      14.375704204071177, 14.731775668541843, 15.101826559104298, 15.487645303540774, 15.891437669317654, 16.315977343186088,
+                      16.764834520627172, 17.242738443345601, 17.756184484560670, 18.314523855092769, 18.932108691320710, 19.633098973347128,
+                      20.464602954108560, 21.548240039455333, 24.164371387589952)
+
   actual_result<-qtriang(q, xMin = xmin, xMax = xmax, xMode = mode)
 
 
@@ -24,7 +24,7 @@ test_that("qtriang", {
   expect_error(qtriang(q, xMin = xmin, xMax = xmax, xMode = 2*xmax), 'xMode must be between xMin and xMax.')
   expect_error(qtriang(q, xMin = xmin, xMax = xmin, xMode = mode), 'xMode must be between xMin and xMax.')
   expect_error(qtriang(q, xMin = xmin, xMax = xmin, xMode = xmin), 'NAs are not allowed in subscripted assignments')
-  expect_equal(actual_result, expected_result, tolerance = 1e-14, scale = 1)
+  expect_equal(actual_result, expected_result, tolerance = 1e-15, scale = 1)
 })
 
 
@@ -34,12 +34,12 @@ test_that("ptriang", {
   mode = rnorm(1,7.5)
   xmin = rnorm(1,1)
   xmax = rnorm(1,9)
-  expected_result = c(0.00000000000000000, 0.00000000000000000, 0.00000000000000000, 0.00251969766858563,
-                      0.01677854605943596, 0.04362561756186622, 0.08306091217587640, 0.13508442990146655,
-                      0.19969617073863660, 0.27689613468738655, 0.36668432174771648, 0.46906073191962633,
-                      0.58402536520311610, 0.71157822159818573, 0.84954213504240184, 0.95101606321399190,
-                      0.99700163646283591, 1.00000000000000000, 1.00000000000000000, 1.00000000000000000,
-                      1.00000000000000000)
+  expected_result = c(0.000000000000000000, 0.000000000000000000, 0.000000000000000000, 0.002519697668585630, 0.016778546059435958,
+                      0.043625617561866219, 0.083060912175876397, 0.135084429901466546, 0.199696170738636603, 0.276896134687386553,
+                      0.366684321747716480, 0.469060731919626328, 0.584025365203116098, 0.711578221598185734, 0.849542135042401836,
+                      0.951016063213991902, 0.997001636462835905, 1.000000000000000000, 1.000000000000000000, 1.000000000000000000,
+                      1.000000000000000000)
+
   actual_result<-ptriang(q, xMin = xmin, xMax = xmax, xMode = mode)
 
 
@@ -52,5 +52,6 @@ test_that("ptriang", {
   expect_error(ptriang(q, xMin = xmin, xMax = xmax, xMode = 2*xmax), 'xMode must be between xMin and xMax.')
   expect_error(ptriang(q, xMin = xmin, xMax = xmin, xMode = mode), 'xMode must be between xMin and xMax.')
   expect_error(ptriang(q, xMin = xmin, xMax = xmin, xMode = xmin), 'NAs are not allowed in subscripted assignments')
-  expect_equal(actual_result, expected_result, tolerance = 1e-14, scale = 1)
+  expect_equal(actual_result, expected_result, tolerance = 1e-15, scale = 1)
 })
+
